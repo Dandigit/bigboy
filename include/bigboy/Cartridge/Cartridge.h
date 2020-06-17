dd4920eaa855afdb16ead4e3ae92690a6a8d3513
Dandigit <37099129+Dandigit@users.noreply.github.com>
2020-06-17 19:54:23 +1000

    Add MBC base to Cartridge

--- include/bigboy/Cartridge/Cartridge.h
+++ include/bigboy/Cartridge/Cartridge.h
@@ -5,16 +5,43 @@
 
 #include <bigboy/MMU/MemoryDevice.h>
 
+enum class MBCType : uint8_t {
+    ROM                     = 0x00,
+    MBC1                    = 0x01,
+    MBC1_RAM                = 0x02,
+    MBC1_RAM_BAATTERY       = 0x03,
+    MBC2                    = 0x05,
+    MBC2_BATTERY            = 0x06,
+    ROM_RAM                 = 0x08,
+    ROM_RAM_BATTERY         = 0x09,
+    MMM01                   = 0x0B,
+    MMM01_RAM               = 0x0C,
+    MMM01_RAM_BATTERY       = 0x0D,
+    MBC3_TIMER_BATTERY      = 0x0F,
+    MBC3_TIMER_RAM_BATTERY  = 0x10,
+    MBC3                    = 0x11,
+    MBC3_RAM                = 0x12,
+    MBC3_RAM_BATTERY        = 0x13,
+    MBC4                    = 0x15,
+    MBC4_RAM                = 0x16,
+    MBC4_RAM_BATTERY        = 0x17,
+    MBC5                    = 0x19,
+    MBC5_RAM                = 0x1A,
+    MBC5_RAM_BATTERY        = 0x1B,
+    MBC5_RUMBLE             = 0x1C,
+    MBC5_RUMBLE_RAM         = 0x1D,
+    MBC5_RUMBLE_RAM_BATTERY = 0x1E,
+    POCKET_CAMERA           = 0xFC,
+    BANDAI_TAMA5            = 0xFD,
+    HUC3                    = 0xFE,
+    HUC1_RAM_BATTERY        = 0xFF,
+};
+
 class Cartridge : public MemoryDevice {
-    std::array<uint8_t, 0x3FFF + 1> m_bank0{0};
-    std::array<uint8_t, 0x3FFF + 1> m_bank1{0};
-
-    std::array<uint8_t, 0x1FFF + 1> m_extRAM{0};
-
 public:
     Cartridge() = default;
-    Cartridge(std::array<uint8_t, 0x3FFF + 1> bank0, std::array<uint8_t, 0x3FFF + 1> bank1);
-    ~Cartridge() = default;
+    Cartridge(const std::vector<uint8_t>& bytes);
+    ~Cartridge();
 
     static Cartridge fromFile(std::string fileName);
 
@@ -24,6 +51,19 @@ public:
     std::vector<AddressSpace> addressSpaces() const override;
     uint8_t readByte(uint16_t address) const override;
     void writeByte(uint16_t address, uint8_t value) override;
+
+private:
+    MBCType getMBCType() const;
+    std::string getGameTitle() const;
+
+    // 0000-3FFF: 16KB ROM Bank 00 (in cartridge, fixed at bank 00)
+    std::array<uint8_t, 0x3FFF + 1> m_bank0{0};
+
+    // 4000-7FFF: 16KB ROM Bank 01..NN (in cartridge, switchable bank number)
+    std::vector<uint8_t> m_bank1Onwards{};
+
+    // A000-BFFF: 8KB External RAM (in cartridge, switchable bank, if any)
+    std::vector<uint8_t> m_extRAM{};
 };
 
 template<typename... T>
@@ -36,7 +76,7 @@ Cartridge Cartridge::test(T... contents) {
         bank0[i + 0x100] = bytes[i];
     }
 
-    return Cartridge{std::move(bank0), std::move(bank1)};
+    return Cartridge{bytes};
 }
 
 #endif //BIGBOY_CARTRIDGE_H
--- lib/Cartridge/Cartridge.cpp
+++ lib/Cartridge/Cartridge.cpp
@@ -1,11 +1,65 @@
 #include <bigboy/Cartridge/Cartridge.h>
 
-#include <iostream>
+#include <algorithm>
 #include <fstream>
+#include <iostream>
 
-Cartridge::Cartridge(std::array<uint8_t, 0x3FFF + 1> bank0, std::array<uint8_t, 0x3FFF + 1> bank1) :
-        m_bank0{std::move(bank0)},
-        m_bank1{std::move(bank1)} {
+Cartridge::Cartridge(const std::vector<uint8_t>& bytes) {
+    if (bytes.size() < m_bank0.size()) {
+        throw std::runtime_error{"Tried to load illegally small ROM."};
+    }
+
+    for (int i = 0; i < m_bank0.size(); i++) {
+        m_bank0[i] = bytes[i];
+    }
+
+    std::copy(bytes.begin() + m_bank0.size(), bytes.end(),
+            std::back_inserter(m_bank1Onwards));
+
+    switch (getMBCType()) {
+        case MBCType::ROM_RAM_BATTERY: {
+            const std::string saveFileName = getGameTitle() + ".gbsave";
+            std::ifstream saveFile{saveFileName};
+            if (saveFile.is_open()) {
+                saveFile.seekg(0, std::ios::end);
+                const size_t length = saveFile.tellg();
+                saveFile.seekg(0, std::ios::beg);
+
+                if (length != 0x2000) {
+                    throw std::runtime_error{"Save file '" + saveFileName + "' is not the right size."};
+                }
+
+                m_extRAM.reserve(0x2000);
+                saveFile.read(reinterpret_cast<char*>(m_extRAM.data()), 0x2000);
+            }
+        }
+        // Fallthrough
+        case MBCType::ROM_RAM:
+        case MBCType::ROM:
+            if (m_bank1Onwards.size() != 0x4000) {
+                throw std::runtime_error{"Tried to load MBC0 ROM that was not 32KB in size."};
+            }
+            break;
+        default:
+            throw std::runtime_error{"Unimplemented MBC type: " + std::to_string(static_cast<int>(getMBCType()))};
+    }
+}
+
+Cartridge::~Cartridge() {
+    switch (getMBCType()) {
+        case MBCType::ROM_RAM_BATTERY: {
+            const std::string saveFileName = getGameTitle() + ".gbsave";
+            std::ofstream saveFile{saveFileName};
+            if (!saveFile.is_open()) {
+                throw std::runtime_error{"Could not write save file '" + saveFileName + "'."};
+            }
+
+            saveFile.write(reinterpret_cast<char*>(m_extRAM.data()), 0x2000);
+            break;
+        }
+        default:
+            break;
+    }
 }
 
 Cartridge Cartridge::fromFile(std::string fileName) {
@@ -16,21 +70,14 @@ Cartridge Cartridge::fromFile(std::string fileName) {
 
     file.seekg(0, std::ios::end);
     size_t length = file.tellg();
-    if (length != 32768) {
-        throw std::runtime_error{"[fatal]: File '" + fileName + "' is not a 32KB GameBoy ROM."};
-    }
-
     file.seekg(0, std::ios::beg);
 
-    std::array<uint8_t, 0x3FFF + 1> bank0{0};
-    file.read(reinterpret_cast<char*>(bank0.data()), bank0.size());
-
-    std::array<uint8_t, 0x3FFF + 1> bank1{0};
-    file.read(reinterpret_cast<char*>(bank1.data()), bank1.size());
-
+    std::vector<uint8_t> bytes{};
+    bytes.resize(length);
+    file.read(reinterpret_cast<char*>(bytes.data()), length);
     file.close();
 
-    return Cartridge{std::move(bank0), std::move(bank1)};
+    return Cartridge{bytes};
 }
 
 std::vector<AddressSpace> Cartridge::addressSpaces() const {
@@ -42,10 +89,23 @@ uint8_t Cartridge::readByte(uint16_t address) const {
         return m_bank0[address];
     }
     if (address >= 0x4000 && address <= 0x7FFF) {
-        return m_bank1[address - 0x4000];
+        switch (getMBCType()) {
+            case MBCType::ROM:
+            case MBCType::ROM_RAM:
+            case MBCType::ROM_RAM_BATTERY:
+                return m_bank1Onwards[address - 0x4000];
+            default:
+                break;
+        }
     }
     if (address >= 0xA000 && address <= 0xBFFF) {
-        return m_extRAM[address - 0xA000];
+        switch (getMBCType()) {
+            case MBCType::ROM_RAM:
+            case MBCType::ROM_RAM_BATTERY:
+                return m_extRAM[address - 0xA000];
+            default:
+                break;
+        }
     }
 
     std::cerr << "Memory device Cartridge does not support reading the address: " << address << '\n';
@@ -54,8 +114,31 @@ uint8_t Cartridge::readByte(uint16_t address) const {
 
 void Cartridge::writeByte(uint16_t address, uint8_t value) {
     if (address >= 0xA000 && address <= 0xBFFF) {
-        m_extRAM[address] = value;
-    } else {
-        std::cerr << "Memory device Cartridge does not support writing to the address: " << address << '\n';
+        switch (getMBCType()) {
+            case MBCType::ROM:
+            case MBCType::ROM_RAM:
+            case MBCType::ROM_RAM_BATTERY:
+                m_extRAM[address - 0xA000] = value;
+                return;
+            default:
+                break;
+        }
     }
+
+    std::cerr << "Memory device Cartridge does not support writing to the address: " << address << '\n';
+}
+
+MBCType Cartridge::getMBCType() const {
+    return static_cast<MBCType>(m_bank0[0x0147]);
+}
+
+std::string Cartridge::getGameTitle() const {
+    std::string name{};
+    for (int i = 0x0134; i < 0x0143; i++) {
+        char c = m_bank0[i];
+        if (c == '\0') break;
+
+        name.push_back(c);
+    }
+    return name;
 }
