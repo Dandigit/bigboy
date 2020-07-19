23d5c7e862afedcdc7a54fd1ad56aca1bcc7bc71
Dandigit <37099129+Dandigit@users.noreply.github.com>
2020-07-19 13:21:39 +1000

    Add MBC3

--- include/bigboy/Cartridge/Cartridge.h
+++ include/bigboy/Cartridge/Cartridge.h
@@ -66,6 +66,61 @@ private:
     bool m_romRamModeSelect = false;
 };
 
+class MBC3 : public Cartridge {
+public:
+    MBC3(std::vector<uint8_t> rom, std::vector<uint8_t> ram, CartridgeHeader header);
+
+    uint8_t readByte(uint16_t address) const override;
+    void writeByte(uint16_t address, uint8_t value) override;
+
+private:
+    // 0000-1FFF: RAM and Timer Enable (write only; lower 4 bits)
+    //  - 00: Disable RAM and Timer (default)
+    //  - 0A: Enable RAM and Timer
+    bool m_ramAndTimerEnable = false;
+
+    // 2000-3FFF: ROM Bank Number (write only)
+    // Same as for MBC1, except that the whole 7 bits of the ROM Bank Number are
+    // written directly to this address. As for the MBC1, writing a value of 00h,
+    // will select Bank 01h instead.
+    uint8_t m_romBankNumber = 0x00;
+
+    // 4000-5FFF: RAM Bank Number or RTC Register Select (write only)
+    // As in MBC1's RAM Banking mode, writing a value in range for 00h-03h maps
+    // the corresponding external RAM Bank (if any) into memory at A000-BFFF. However,
+    // when writing a value of 08h-0Ch, this will map the corresponding RTC register
+    // into memory at A000-BFFF. That register could then be read/written by accessing
+    // any address in that area, typically that is done by using address A000.
+    uint8_t m_ramBankNumberOrRtcRegisterSelect = 0x00;
+
+    // 6000-7FFF: Latch Clock Data (write only)
+    // When writing 00h, and then 01h to this register, the current time becomes latched
+    // into the RTC registers. The latched data will not change until it becomes latched
+    // again, by repeating the write 00h->01h procedure. It is provided so that the latched
+    // (frozen) time may be read while the clock continues to tick in the background.
+    uint8_t m_latchClockData = 0x02;
+
+    // The Clock Counter Registers (read/write)
+    //  - 08h  RTC S   Seconds   0-59 (0-3Bh)
+    uint8_t m_rtcSeconds = 0x00;
+    //  - 09h  RTC M   Minutes   0-59 (0-3Bh)
+    uint8_t m_rtcMinutes = 0x00;
+    //  - 0Ah  RTC H   Hours     0-23 (0-17h)
+    uint8_t m_rtcHours = 0x00;
+    //  - 0Bh  RTC DL  Lower 8 bits of Day Counter (0-FFh)
+    uint8_t m_dayCounterLower = 0x00;
+    //  - 0Ch  RTC DH  Upper 1 bit of Day Counter, Carry Bit, Halt Flag
+    //    - Bit 0  Most significant bit of Day Counter (Bit 8)
+    //    - Bit 6  Halt (0=Active, 1=Stop Timer)
+    //    - Bit 7  Day Counter Carry Bit (1=Counter Overflow)
+    uint8_t m_dayCounterHigher = 0x00;
+
+    // Notes: The Day Counter
+    // The total 9 bits of the Day Counter allow to count days in range from 0-511 (0-1FFh).
+    // The Day Counter Carry Bit becomes set when this value overflows. In that case the Carry
+    // Bit remains set until the program resets it.
+};
+
 class MBC5 : public Cartridge {
 public:
     MBC5(std::vector<uint8_t> rom, std::vector<uint8_t> ram, CartridgeHeader header);
